c780ecc40c955d2133813c07e94c9f588c84bbba
Cam <64113264+RookWorks@users.noreply.github.com>
2022-04-15 20:11:25 -0400

    boiler/basic stuff

---
+++ workingPage.js
@@ -0,0 +1,8 @@
+//DOM Elements
+const time = document.getElementById('times'),
+    greeting = document.getElementById('greeting'),
+    name = document.getElementById ('name'),
+    focus = document.getElementById('focus');
+
+//Array(s)
+
