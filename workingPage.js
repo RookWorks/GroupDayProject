89fbc73dd1cac99df8ab73cefc641b4026603e32
jaynou <93495926+jaynou@users.noreply.github.com>
2022-04-20 18:47:46 -0400

    Logic update

--- workingPage.js
+++ workingPage.js
@@ -13,3 +13,54 @@ const time = document.getElementById('times'),
 // };
 
 //This is a test merge
+
+// Get Focus
+function getFocus() {
+    if (localStorage.getItem('focus') === null) {
+        focus.textContent = '[Enter Focus]';
+    } else {
+        focus.textContent =localStorage.getItem('focus');
+    }
+}
+
+// Set Focus
+function setFocus(e) {
+    if (e.type === 'keypress') {
+        // Make sure enter key is pressed
+        if (e.which == 13 || e.keycode == 13) {
+            localStorage.setItem('focus', e.target.innerText);
+            focus.blur();
+        } else {
+            localStorage.setItem('focus', e.target.innerText);
+    }
+}
+
+// Get Name
+function getName() {
+    if (localStorage.getItem('name') === null) {
+        name.textContent = '[Enter Name]';
+    } else {
+        name.textContent = localStorage.getItem('name');
+    }
+}
+
+// Set Name
+function setName(e) {
+    if (e.type === 'keypress') {
+        if (e.which == 13 || e.keycode == 13) {
+            localStorage.setItem('name', e.target.innerText);
+            name.blur();
+        } else {
+            localStorage.setItem('name', e.target.innerText);
+        }
+    }
+}
+
+
+name.addEventListener('keypress', setName);
+name.addEventListener('blur', setName);
+focus.addEventListener('keypress', setFocus);
+focus.addEventListener('blur', setFocus);
+
+
+
